6ecdda0794f887d5d7968aa778949e81291c6c74
kika1s1 <tamiratkebede120@gmail.com>
2024-07-20 14:48:48 +0300

    create signin api route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,47 +1,53 @@
-import bcryptjs from 'bcryptjs'
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import User from "../models/User.js";
-import ErrorResponse from '../utils/errorResponse.js';
-
+import ErrorResponse from "../utils/errorResponse.js";
+dotenv.config();
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  if(!(username && email && password)){
-    return next(new ErrorResponse("All fields are required.", 400))
+  if (!(username && email && password)) {
+    return next(new ErrorResponse("All fields are required.", 400));
   }
-  const hashedPassword =  bcryptjs.hashSync(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
-    const userEmail = await User.findOne({email})
-    const userUsername = await User.findOne({username})
-    if(userEmail){
-        return next(new ErrorResponse("Email already exists.", 400))
+    const userEmail = await User.findOne({ email });
+    const userUsername = await User.findOne({ username });
+    if (userEmail) {
+      return next(new ErrorResponse("Email already exists.", 400));
     }
-    if(userUsername){
-      return next(new ErrorResponse("Username already exists.", 400))
+    if (userUsername) {
+      return next(new ErrorResponse("Username already exists.", 400));
     }
     await newUser.save();
     res.status(201).json({
-        message:"User created successfully!"
+      message: "User created successfully!",
     });
   } catch (error) {
     next(error);
   }
 };
 
-export const signin = async (req, res) => {
+export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const user = await User.find({ email });
+    const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "User not found" });
+      return next(new ErrorResponse("User not found", 404));
     }
-    const isMatch = await user.comparePassword(password);
+    const isMatch = await bcrypt.compare(password, user.password);
+
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid password" });
+      return next(new ErrorResponse("Invalid credentials", 400));
     }
-    const token = user.generateToken();
-    res.cookie("token", token, { httpOnly: true });
-    res.status(200).json({ message: "User signed in", user: user });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: pwd, ...userData } = user._doc;
+    res
+      .cookie("x-auth-token", token, { httpOnly: true })
+      .status(200)
+      .json(userData);
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    next(error);
   }
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.js";
+import { signup,signin } from "../controllers/auth.js";
 
 const router = Router();
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 
 export default router;
