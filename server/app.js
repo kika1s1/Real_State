631ecbcbedee2aba7bbafe476891c96563e47726
kika1s1 <tamiratkebede120@gmail.com>
2024-07-20 12:04:50 +0300

    connect databases

--- server/app.js
+++ server/app.js
@@ -1,16 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
-
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const PORT = process.env.PORT || 3000
 const app = express();
-
 app.use(express.json());
 
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000!");
 });
-
-
