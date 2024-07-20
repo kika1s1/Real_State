e5d60c7f73ec7244f81a57d12306a42c5dd0cf26
kika1s1 <tamiratkebede120@gmail.com>
2024-07-20 17:28:30 +0300

    making profile picture clickable

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,14 @@
 import {useSelector} from 'react-redux'
-
+import { useRef } from 'react'
 const  Profile =()=>{
+  const fileRef = useRef()
   const {currentUser} = useSelector((state) => state.user)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+        <input hidden type="file" name="" ref={fileRef} id="avatar" accept='image/*' />
+        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
         <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg'  />
         <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg'  />
         <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg'  />
