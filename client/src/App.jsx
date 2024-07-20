77ebe8b44daa47b5a24c883ac41b9e3cd7e2a5bf
kika1s1 <tamiratkebede120@gmail.com>
2024-07-20 10:56:21 +0300

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
 const App = () => {
   return (
-    <div className="text-3xl underline ">App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+
+const Signup = () => {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default Signup
