825e5b2a2f6ddaeb64f942e170dbd136594cc226
kika1s1 <tamiratkebede120@gmail.com>
2024-07-22 14:19:01 +0300

    admin identified and getUser controller is added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -54,11 +54,16 @@ export const updateUser = async (req, res, next) => {
 
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id)
-    return next(new ErrorResponse('You can only delete your own account!', 401));
+  
   try {
+    const user = await User.findById(req.user.id)
+    if (req.user.id !== req.params.id && !user.isAdmin)
+      return next(new ErrorResponse('You can only delete your own account!', 401));
+
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('x-auth-token');
+    if(req.user.id === req.params.id){
+      res.clearCookie('x-auth-token');
+    }
     res.status(200).json('User has been deleted!');
   } catch (error) {
     next(error);
@@ -91,6 +96,51 @@ export const getUser = async (req, res, next) => {
     const { password: pass, ...rest } = user._doc;
   
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  
+  try {
+    const user = await User.findById(req.user.id)
+    if (!user.isAdmin) {
+      return next(new ErrorResponse('You are not allowed to see all users', 403));
+    }
+
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
   } catch (error) {
     next(error);
   }
