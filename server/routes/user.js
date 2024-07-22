42ef8194999575c676205fc92db394d9a4252f37
kika1s1 <tamiratkebede120@gmail.com>
2024-07-22 14:16:16 +0300

    get users route is added

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { deleteUser, getUser, getUserListings, test, updateUser } from '../controllers/user.js'
+import { deleteUser, getUser, getUserListings, getUsers, test, updateUser } from '../controllers/user.js'
 import upload from "../config/multer.js";
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -9,5 +9,6 @@ router.get("/test",test)
 router.post('/update/:id',verifyToken, upload.single('profileImage'), updateUser); 
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/getusers', verifyToken, getUsers);
 router.get('/:id', getUser)
 export default router
