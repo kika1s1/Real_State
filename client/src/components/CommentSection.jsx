bd05696e76ebd1e7960af66428c2bc313b7913ea
kika1s1 <tamiratkebede120@gmail.com>
2024-07-22 15:24:45 +0300

    comment section UI is fixed

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -113,7 +113,7 @@ export default function CommentSection({ postId }) {
     }
   };
   return (
-    <div className="max-w-2xl mx-auto w-full p-3">
+    <div className="">
       {currentUser ? (
         <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
           <p>Signed in as:</p>
@@ -143,7 +143,7 @@ export default function CommentSection({ postId }) {
           className="border border-teal-500 rounded-md p-3"
         >
           <Textarea
-            placeholder="Add a comment..."
+            placeholder="Write a comment..."
             rows="3"
             maxLength="200"
             onChange={(e) => setComment(e.target.value)}
