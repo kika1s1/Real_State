c0e67ad44c952b625261d859800a37465ed7c1d1
kika1s1 <tamiratkebede120@gmail.com>
2024-09-19 21:24:37 +0300

    promote demote added

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,14 +1,14 @@
 import User from '../models/User.js'; 
 import ErrorResponse from '../utils/errorResponse.js'; 
 
-export const promoteAdmin = async (req, res, next) => {
+export const promoteDemoteAdmin = async (req, res, next) => {
     const { id } = req.params;
     try {
         const user = await User.findById(id);
         if (!user) {
             return next(new ErrorResponse("User not found", 404));
         }
-        user.isAdmin = true;
+        user.isAdmin = !user.isAdmin;
         await user.save();
         res.status(200).json({
             "message": "User has been promoted to admin",
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,9 +1,9 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
 import verifyAdmin from "../middleware/adminMiddleware.js"
-import { promoteAdmin } from "../controllers/admin.js"
+import { promoteDemoteAdmin } from "../controllers/admin.js"
 const router = express.Router()
 
-router.patch("/promote/:id", verifyToken, verifyAdmin, promoteAdmin)
+router.patch("/promote_demote/:id", verifyToken, verifyAdmin, promoteDemoteAdmin)
 
 export default router
