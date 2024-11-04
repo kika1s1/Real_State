1101df6dddb3b80056fab079f3b5223fa6519661
kika1s1 <tamiratkebede120@gmail.com>
2024-11-04 10:34:05 +0300

    serving static file for build is added

--- server/app.js
+++ server/app.js
@@ -65,6 +65,12 @@ app.use('/api/listing', listingRoutes);
 app.use('/api/comment', commentRoutes);
 app.use("/api/admin", adminRoutes);
 app.use(errorHandler);
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log("Server is running on port 3000!");
